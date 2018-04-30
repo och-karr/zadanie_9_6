b801f599c71cd0edfdc54b35cdde4b9670b7d8b4
karolinneczka <okaa93@gmail.com>
2018-04-30 22:39:26 +0200

    Add player selection simple structure

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,10 @@ var newGameBtn = document.getElementById('js-newGameButton');
 
 newGameBtn.addEventListener('click', newGame);
 
+var pickRock = document.getElementById('js-playerPick_rock');
+var pickPaper = document.getElementById('js-playerPick_paper');
+var pickScissors = document.getElementById('js-playerPick_scissors');
+
+pickRock.addEventListener('click', function() { playerPick('rock') });
+pickPaper.addEventListener('click', function() { playerPick('paper') });
+pickScissors.addEventListener('click', function() { playerPick('scissors') });
