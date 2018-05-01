c625ce5eab529ae7bcc15a63fc50c86ed330d9a4
karolinneczka <okaa93@gmail.com>
2018-05-01 16:19:23 +0200

    Define newGame function which starts new game

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,20 @@ function setGameElements() {
 }
 
 setGameElements();
+
+var playerPointsElem = document.getElementById('js-playerPoints'),
+    playerNameElem = document.getElementById('js-playerName'),
+    computerPointsElem = document.getElementById('js-computerPoints');
+
+function newGame() {
+  player.name = prompt('Please enter your name', 'imię gracza');
+  if (player.name) {
+    player.score = computer.score = 0;
+    gameState = 'started';
+    setGameElements();
+
+    playerNameElem.innerHTML = player.name;
+    // setGamePoints(); // This function has not been created yet
+  }
+
+}
