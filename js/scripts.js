eb5b1fca3dfb6dfb0ffb39ccbf8b408672ad426d
karolinneczka <okaa93@gmail.com>
2018-05-01 15:23:26 +0200

    Add padding in css, add initial values and set displaying game elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 var newGameBtn = document.getElementById('js-newGameButton');
-
 newGameBtn.addEventListener('click', newGame);
 
 var pickRock = document.getElementById('js-playerPick_rock');
@@ -9,3 +8,35 @@ var pickScissors = document.getElementById('js-playerPick_scissors');
 pickRock.addEventListener('click', function() { playerPick('rock') });
 pickPaper.addEventListener('click', function() { playerPick('paper') });
 pickScissors.addEventListener('click', function() { playerPick('scissors') });
+
+var gameState = 'notStarted';  //started // ended
+var player = {
+        name: '',
+        score: 0
+    };
+var computer = {
+        score: 0
+    };
+
+var newGameElem = document.getElementById('js-newGameElement');
+var pickElem = document.getElementById('js-playerPickElement');
+var resultsElem = document.getElementById('js-resultsTableElement');
+
+function setGameElements() {
+  switch(gameState) {
+    case 'started':
+        newGameElem.style.display = 'none';
+        pickElem.style.display = 'block';
+        resultsElem.style.display = 'block';
+      break;
+    case 'ended':
+        newGameBtn.innerText = 'Jeszcze raz';
+    case 'notStarted':
+    default:
+        newGameElem.style.display = 'block';
+        pickElem.style.display = 'none';
+        resultsElem.style.display = 'none';
+  }
+}
+
+setGameElements();
